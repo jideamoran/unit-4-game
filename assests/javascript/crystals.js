07729e022e7c13bf5575fd2b52a38b81dfaebdb2
plusultra <jideamoran@gmail.com>
2019-03-05 10:02:45 -0500

    trying to update, lost page, need to fix

--- assests/javascript/crystals.js
+++ assests/javascript/crystals.js
@@ -1,50 +1,61 @@
-function startGame() {
-
-    var = crystalNumber 
-    var = crystal1
-    var = crystal2
-    var = crystal3
-    var = crystal4
-    var = totalscore
-    var = wins
-    var = losses
+    var crystal1
+    var crystal2
+    var crystal3
+    var crystal4
+    var totalscore = 0
+    
+   
 
 
-     crystalNumber return Math.floor(Math.random() * 12 -1 + 1) + min);
+
+     var crystalNumber = Math.floor(Math.random() * 12 - 1+ 1
+
+  $("#crystal").text(number);
+
+  console.log(crystalNumber)
 
         (function(){ randomnumber Math.floor(Math.random() * 12 -1 + 1) + min);
 
             (function(){ crystalNumber return Math.floor(Math.random() * 12 -1 + 1) + min);
-
+s
                (function(){ crystalNumber return Math.floor(Math.random() * 12 -1 + 1) + min);
 
-console.log 
 
-    $("crystalNumber").onclick.push (min: 19, max: 120) {
 
-        return [Math.floor(Math.random() * max - min + 1) + min)];
-    }
+                $("#crystal1", "#crtystal2", "#crystal3", "#crystal4").onclick(function() 
 
-    $('#crystal1').onclick; crystal1(min: 1, max: 12) {
+                    (min: 1, max: 12) {
 
-        return Math.floor(Math.random()* max - min + 1)+ min);
-    }
-    $('#crystal2').onclick.innerhtml crystal2(min: 1, max: 12) {
+                        Math.floor(Math.random()* max - min + 1)+ min);
+                    }
+player .onclick
 
-        return Math.floor(Math.random()* max - min + 1)+ min);
-    }
-    $('#crystal3').onclick  crystal3(min: 1, max: 12) {
+    $("#crystalNumber").onclick.push (min: 19, max: 120) {
 
-        return Math.floor(Math.random()* max - min + 1)+ min);
-    }
-    $('#crystal4').onclick crystal4.(min: 1, max: 12) {
+         [Math.floor(Math.random() * max - min + 1) + min)];
+    });
 
-        return Math.floor(Math.random()* max - min + 1)+ min);
-    }
+    $("#crystal1").onclick; crystal1 (min: 1, max: 12) {
+
+         Math.floor(Math.random()* max - min + 1)+ min);
+    });
+    $("#crystal2").onclick.innerhtml crystal2(min: 1, max: 12) {
+
+         Math.floor(Math.random()* max - min + 1)+ min);
+    });
+    $("#crystal3").onclick  crystal3(min: 1, max: 12) {
+
+         Math.floor(Math.random()* max - min + 1)+ min);
+    });
+    $(("#crystal4").onclick crystal4.(min: 1, max: 12) {
+
+         Math.floor(Math.random()* max - min + 1)+ min);
+    });
 
 
 console.log
-    
+
+
 
    
 
