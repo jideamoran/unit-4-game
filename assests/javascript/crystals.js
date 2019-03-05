92e44acd262de444561f486145873a63dd770fd7
plusultra <jideamoran@gmail.com>
2019-03-04 19:35:58 -0500

    working with js, adding buttons

--- assests/javascript/crystals.js
+++ assests/javascript/crystals.js
@@ -1,1 +1,52 @@
-crystals.js
+function startGame() {
+
+    var = crystalNumber 
+    var = crystal1
+    var = crystal2
+    var = crystal3
+    var = crystal4
+    var = totalscore
+    var = wins
+    var = losses
+
+
+     crystalNumber return Math.floor(Math.random() * 12 -1 + 1) + min);
+
+        (function(){ randomnumber Math.floor(Math.random() * 12 -1 + 1) + min);
+
+            (function(){ crystalNumber return Math.floor(Math.random() * 12 -1 + 1) + min);
+
+               (function(){ crystalNumber return Math.floor(Math.random() * 12 -1 + 1) + min);
+
+console.log 
+
+    $("crystalNumber").onclick.push (min: 19, max: 120) {
+
+        return [Math.floor(Math.random() * max - min + 1) + min)];
+    }
+
+    $('#crystal1').onclick; crystal1(min: 1, max: 12) {
+
+        return Math.floor(Math.random()* max - min + 1)+ min);
+    }
+    $('#crystal2').onclick.innerhtml crystal2(min: 1, max: 12) {
+
+        return Math.floor(Math.random()* max - min + 1)+ min);
+    }
+    $('#crystal3').onclick  crystal3(min: 1, max: 12) {
+
+        return Math.floor(Math.random()* max - min + 1)+ min);
+    }
+    $('#crystal4').onclick crystal4.(min: 1, max: 12) {
+
+        return Math.floor(Math.random()* max - min + 1)+ min);
+    }
+
+
+console.log
+    
+
+   
+
+
+
